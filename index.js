907999c381541490e3ba08691444fda23b181eb4
Nick Strong <nstrong92@yahoo.com>
2021-05-11 17:32:03 -0600

    linked inquirer, modified gitignore, started on readme questions array

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  "What is your project title?",
+  "Please describe your project",
+  "Please list table of contents (optional)",
+  "Instalation",
+  "What is the applications usage?",
+  "Did you use any outside sources you would like to credit?",
+  "Any licenseing?",
+  "Features?",
+  "Tests?",
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
