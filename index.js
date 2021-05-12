b47f133867c844d0532fbd8aa5c2550c2d8036ea
Nick Strong <nstrong92@yahoo.com>
2021-05-11 19:49:58 -0600

    added in questions to index.js, started writeToFile function

--- index.js
+++ index.js
@@ -6,16 +6,70 @@ const questions = [
   "What is your project title?",
   "Please describe your project",
   "Please list table of contents (optional)",
-  "Instalation",
+  "How do you install this application?",
   "What is the applications usage?",
   "Did you use any outside sources you would like to credit?",
   "Any licenseing?",
-  "Features?",
-  "Tests?",
+  "What features does your application have?",
+  "What tests were run during this applications creation?",
 ];
 
+inquirer
+  .prompt([
+    {
+      name: "projectName",
+      type: "input",
+      message: "What is your project title?",
+    },
+    {
+      name: "Description",
+      type: "input",
+      message: "Please describe your project",
+    },
+    {
+      name: "tableOfContents",
+      type: "input",
+      message: "Please list table of contents (optional)",
+    },
+    {
+      name: "Installation",
+      type: "input",
+      message: "How do you install this application?",
+    },
+    {
+      name: "appUsage",
+      type: "input",
+      message: "What is the applications usage?",
+    },
+    {
+      name: "credits",
+      type: "input",
+      message: "Did you use any outside sources you would like to credit?",
+    },
+    {
+      name: "licensing",
+      type: "input",
+      message: "Any licenseing?",
+    },
+    {
+      name: "features",
+      type: "input",
+      message: "What features does your application have?",
+    },
+    {
+      name: "tests",
+      type: "input",
+      message: "What tests were run during this applications creation?",
+    },
+  ])
+  .then((response) => {
+    console.log(response);
+  });
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  const fs = require("fs");
+}
 
 // TODO: Create a function to initialize app
 function init() {}
